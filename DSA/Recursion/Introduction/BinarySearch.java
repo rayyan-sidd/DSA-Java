28d2ba4769573eba642c01514a0ed26d4c76beae
rayyan-sidd <rayyanplaysrgy@gmail.com>
2025-08-02 15:35:04 +0530

    Learnt Basics of Recursion and Recusrive tree

---
+++ DSA/Recursion/Introduction/BinarySearch.java
@@ -0,0 +1,24 @@
+package DSA.Recursion.Introduction;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 5, 3, 33, 44};
+        int target = (33);
+        System.out.println(search(arr, target, 0, arr.length-1));
+    }
+
+    static int search(int[] arr, int target, int start, int end){
+        if(start > end)
+            return -1;
+        
+        int mid = start + (end - start)/2;
+        if(arr[mid] == target){
+            return mid;
+        }
+        else if(target < arr[mid]){
+            return search(arr, target, start, mid-1);
+        }
+        
+        return search(arr, target, mid+1, end);
+    }
+}
